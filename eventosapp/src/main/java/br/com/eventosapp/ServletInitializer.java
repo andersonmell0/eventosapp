3a597f01a5657c8b43772160bc3d4c8fa6d852d0
mello <mello@LAPTOP-EFEPHNV0>
2018-04-23 22:43:59 -0300

    Commit Inicial do Projeto

---
+++ eventosapp/src/main/java/br/com/eventosapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.eventosapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EventosappApplication.class);
+	}
+
+}
